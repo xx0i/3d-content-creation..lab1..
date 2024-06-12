634fe51429d65c23a9fd06a6295305347c8d59ef
Lari <lnorri.fullsail@gmail.com>
2024-06-12 18:35:30 -0400

    auto commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,57 @@
-#include <iostream>
-
-int main() {
-	std::cout << "Hello World!" << std::endl;
+// Simple basecode showing how to create a window and attatch a vulkansurface
+#define GATEWARE_ENABLE_CORE // All libraries need this
+#define GATEWARE_ENABLE_SYSTEM // Graphics libs require system level libraries
+#define GATEWARE_ENABLE_GRAPHICS // Enables all Graphics Libraries
+// Ignore some GRAPHICS libraries we aren't going to use
+#define GATEWARE_DISABLE_GDIRECTX11SURFACE // we have another template for this
+#define GATEWARE_DISABLE_GDIRECTX12SURFACE // we have another template for this
+#define GATEWARE_DISABLE_GRASTERSURFACE // we have another template for this
+#define GATEWARE_DISABLE_GOPENGLSURFACE // we have another template for this
+// With what we want & what we don't defined we can include the API
+#include "Gateware.h"
+#include "FileIntoString.h"
+#include "renderer.h"
+// open some namespaces to compact the code a bit
+using namespace GW;
+using namespace CORE;
+using namespace SYSTEM;
+using namespace GRAPHICS;
+// lets pop a window and use Vulkan to clear to a red screen
+int main()
+{
+	GWindow win;
+	GEventResponder msgs;
+	GVulkanSurface vulkan;
+	if (+win.Create(0, 0, 800, 600, GWindowStyle::WINDOWEDBORDERED))
+	{
+		// TODO: Part 1a
+        VkClearColorValue clr = { {0.75f, 0, 0, 1} };// TODO: Part 1a
+		msgs.Create([&](const GW::GEvent& g) {
+			GW::SYSTEM::GWindow::Events q;
+			if (+g.Read(q) && q == GWindow::Events::RESIZE)
+				clr.float32[2] += 0.01f;// TODO: Part 1a
+			});
+		win.Register(msgs);
+#ifndef NDEBUG
+		const char* debugLayers[] = {
+			"VK_LAYER_KHRONOS_validation", // standard validation layer
+		};
+		if (+vulkan.Create(	win, 0, sizeof(debugLayers)/sizeof(debugLayers[0]), 
+							debugLayers, 0, nullptr, 0, nullptr, false))
+#else
+		if (+vulkan.Create(win, 0))
+#endif
+		{
+			Renderer renderer(win, vulkan);
+			while (+win.ProcessWindowEvents())
+			{
+				if (+vulkan.StartFrame(1, clr.float32))
+				{
+					renderer.Render();
+					vulkan.EndFrame(true);
+				}
+			}
+		}
+	}
+	return 0; // that's all folks
 }
