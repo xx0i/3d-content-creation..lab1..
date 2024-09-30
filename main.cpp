a29f3615ba8974d750d52e17790eda19c86d25da
unknown <natalie9705@gmail.com>
2024-09-30 13:02:04 -0400

    auto commit

--- main.cpp
+++ main.cpp
@@ -25,11 +25,13 @@ int main()
 	if (+win.Create(0, 0, 800, 600, GWindowStyle::WINDOWEDBORDERED))
 	{
 		// TODO: Part 1a
-        VkClearColorValue clr = { {0, 0, 0, 0} };// TODO: Part 1a
+        VkClearColorValue clr = { {0, 0, 0, 1} };// TODO: Part 1a
 		msgs.Create([&](const GW::GEvent& g) {
 			GW::SYSTEM::GWindow::Events q;
 			if (+g.Read(q) && q == GWindow::Events::RESIZE)
-				clr.float32[2] += 1.0f;// TODO: Part 1a
+				clr.float32[0] += 1.0f;// TODO: Part 1a
+				clr.float32[1] += 1.0f;
+				clr.float32[2] += 1.0f;
 				win.SetWindowName("Natalie Lubahn - Lab 1 - Vulkan");
 				
 			});
