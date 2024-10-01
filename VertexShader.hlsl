f8be947de2cf37f7a79f5b20c6e9cc6acca99b44
unknown <natalie9705@gmail.com>
2024-10-01 12:01:14 -0400

    auto commit

--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -2,14 +2,14 @@
 struct VS_OUTPUT
 {
     float4 pos : SV_POSITION;
-    //[[vk::builtin("PointSize")]] // uncomment for point size
-    //float PointSize : POINT_SIZE; // uncomment for point size
+    [[vk::builtin("PointSize")]] // uncomment for point size
+    float PointSize : POINT_SIZE; // uncomment for point size
 };
 
 VS_OUTPUT main(float2 inputVertex : POSITION)
 {
     VS_OUTPUT output;
     output.pos = float4(inputVertex, 0, 1);
-    //output.PointSize = 2.0f; // uncomment for point size
+    output.PointSize = 2.0f; // uncomment for point size
     return output;
 }
