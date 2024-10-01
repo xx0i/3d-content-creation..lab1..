2a7cead5a5c441f9e334e10ace430e59fa9447dd
unknown <natalie9705@gmail.com>
2024-10-01 14:03:03 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -387,8 +387,12 @@ private:
 		pipeline_create_info_star.subpass = 0;
 		pipeline_create_info_star.basePipelineHandle = VK_NULL_HANDLE;
 
-		vkCreateGraphicsPipelines(
+		VkResult result =  vkCreateGraphicsPipelines(
 			device, VK_NULL_HANDLE, 1, &pipeline_create_info_star, nullptr, &starPipeline);
+		if (result != VK_SUCCESS) {
+			std::cerr << "Failed to create star pipeline: " << result << std::endl;
+		}
+
 		// TODO: Part 4f
 		VkVertexInputBindingDescription bindingDescription{};
 		bindingDescription.binding = 0;
