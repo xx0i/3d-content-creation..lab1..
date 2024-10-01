1f66e3e62c75a21a9bdbf0f2bd8584a0895462f9
unknown <natalie9705@gmail.com>
2024-10-01 17:49:04 -0400

    auto commit

--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -10,6 +10,6 @@ VS_OUTPUT main(float2 inputVertex : POSITION)
 {
     VS_OUTPUT output;
     output.pos = float4(inputVertex, 0, 1);
-    output.PointSize = 2.0f; // uncomment for point size
+    output.PointSize = 1.0f; // uncomment for point size
     return output;
 }
